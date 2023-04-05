05a65a4b3dc1a18c7d94aed7afe19e53375d7c1d
NMoskvi4eva <74330528+NMoskvi4eva@users.noreply.github.com>
2023-04-05 21:44:18 +0300

    Update script.js

--- sweets/js/script.js
+++ sweets/js/script.js
@@ -1,18 +1,39 @@
-let offset = 0;
-const sliderLine = document.querySelector('.slider-line');
+const images = document.querySelectorAll('.slider .slider-line .review');
+const sliderLine = document.querySelector('.slider .slider-line');
+let count = 0;
+let width;
 
-document.querySelector('.slider-next').addEventListener('click', function(){
-    offset = offset + 1180;
-    if (offset > 2700) {
-        offset = 0;
+function init() {
+    console.log('resize');
+    width = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollSlider();
+}
+
+init();
+window.addEventListener('resize', init);
+
+document.querySelector('.slider-next').addEventListener('click', function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
     }
-    sliderLine.style.left = -offset + 'px';
+    rollSlider();
 });
 
 document.querySelector('.slider-prev').addEventListener('click', function () {
-    offset = offset - 1180;
-    if (offset < 0) {
-        offset = 2700;
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
     }
-    sliderLine.style.left = -offset + 'px';
+    rollSlider();
 });
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+
+}
